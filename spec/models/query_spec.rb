a42a9386aecb9f4f0d32d5a0c70df625316c19af
Jon Allured <jon.allured@gmail.com>
2016-10-31 14:19:21 -0500

    Add basic unit test around creating queries
    This test uses the seeds file to ensure that we're accounting for all
    Services.

---
+++ spec/models/query_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Query' do
+  describe 'performing checks' do
+    it 'enqueues checks for all Services' do
+      Rails.application.load_seed
+      adapter = ActiveJob::Base.queue_adapter
+
+      query = Query.create term: 'hashrocket'
+
+      expect(query.checks.count).to eq 1
+      expect(adapter.enqueued_jobs.count).to eq 1
+    end
+  end
+end
